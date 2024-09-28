2b25d1620f7271c08be5c36006cc1da45c93fbb9
whoisPratish <pratish.dhakal2059@gmail.com>
2024-09-27 23:53:51 -0400

    Finished backend, now onto frontend

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ const cookieParser = require('cookie-parser');
 const mongoose = require('mongoose');
 const port = 5000;
 
+app.use(cookieParser());
+app.use(express.urlencoded({extended: true}))
+app.set('view engine', 'ejs');
+
 const uri = 'mongodb://localhost:27017/my-database';
 
 const connectToDb = async ()=>{
@@ -18,3 +22,95 @@ const connectToDb = async ()=>{
 }
 connectToDb();
 
+const userSchema = mongoose.Schema({
+    username : String,
+    password : String,
+    role : String
+})
+
+
+const User = mongoose.model('User', userSchema);
+
+
+const isTeacher = (req,res,next)=>{
+    if(req.cookies.role === 'teacher'){
+        next()
+    } else{
+        res.send('You must be a teacher to view this page.')
+    }
+}
+
+const isAthenticated = (req,res,next)=>{
+    if (req.cookies.auth === 'true'){
+        next();
+    }
+    else {
+        res.send('Please sign in to view the page')
+    }
+}
+
+app.get('/', (req,res)=>{
+    res.render('home')
+})
+
+app.get('/dashboard', isAthenticated, (req,res)=>{
+    res.render('dashboard')
+})
+
+app.get('/contact',isAthenticated, (req,res)=>{
+    res.render('contact')
+})
+
+app.get('/service', isAthenticated, (req,res)=>{
+    res.render('service')
+})
+
+app.get('/teacher', isAthenticated, isTeacher, (req,res)=>{
+    res.render('teacher')
+})
+
+app.post('/login', async (req,res)=>{
+    const {username, password, role} = req.body;
+     
+    const existingUser = await User.findOne({username, password})
+    if (existingUser){
+        res.cookie('auth', 'true');
+        res.cookie('role', existingUser.role);
+        res.redirect('/dashboard')
+    }
+    else {
+        res.send('Login credential error...')
+    }
+
+})
+
+app.post('/register', async (req,res)=>{
+    const {username, password, role} = req.body;
+
+    const existingUser = await User.findOne({username});
+    if (existingUser){
+        res.send('Username already taken')
+    }
+    else {
+        const newUser = new User({
+            username,
+            role,
+            password
+        })
+        await newUser.save();
+        res.cookie('auth', 'true');
+        res.cookie('role', role);
+        res.redirect('/dashboard')
+    }
+})
+
+app.post('/logout', (req,res)=>{
+    res.clearCookie('auth', 'true');
+    res.clearCookie('role', role);
+    res.redirect('/')
+})
+
+app.listen(port, ()=>{
+    console.log(`Server is running in port ${port}`);
+    
+})
